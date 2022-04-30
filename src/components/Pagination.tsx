701ed810af91eda2e45f4a3705aa9537908fda55
Shannon <sh4nnongoh@gmail.com>
2022-04-30 19:20:38 +0800

    [Feature] Add pagination tests.

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -8,6 +8,10 @@ const Pagination: FC<{
   setPage
 }) => {
   const { PoolCardList } = useContext(CoinHallFilterSortContext);
+  const buttonDisableClassName = "flex items-center space-x-1 py-2 px-4 rounded"
+  + " bg-gray-800 transition duration-100 text-gray-500 cursor-default";
+  const buttonEnabledClassName = "py-2 px-4 bg-gray-800 rounded-r hover:text-violet-300"
+  + " hover:bg-violet-800 hover:bg-opacity-30 transition duration-100";
   return (
     <div className="flex justify-end items-center mt-4 text-sm sm:text-base">
       <div className="flex items-center space-x-1 select-none">
@@ -15,8 +19,7 @@ const Pagination: FC<{
           type="button"
           disabled={page === 1}
           onClick={() => { setPage(page - 1); }}
-          className="flex items-center space-x-1 py-2 px-3 rounded bg-gray-800
-          transition duration-100 text-gray-500 cursor-default"
+          className={page === 1 ? buttonDisableClassName : buttonEnabledClassName}
         >
           {"< Previous"}
         </button>
@@ -27,8 +30,7 @@ const Pagination: FC<{
           type="button"
           disabled={PoolCardList.length < page * 24}
           onClick={() => { setPage(page + 1); }}
-          className="flex items-center space-x-1 py-2 px-3 rounded bg-gray-800
-          transition duration-100 text-gray-500 cursor-default"
+          className={PoolCardList.length < page * 24 ? buttonDisableClassName : buttonEnabledClassName}
         >
           {"Next >"}
         </button>
